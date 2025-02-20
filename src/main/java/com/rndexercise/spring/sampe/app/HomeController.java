fbb48f0e05d4cd5487fefa88a9591372f79e94d7
sandeep Rayala <nayakanti.renuka@gmail.com>
2025-02-20 09:42:00 +0530

    cicd exercise

---
+++ src/main/java/com/rndexercise/spring/sampe/app/HomeController.java
@@ -0,0 +1,12 @@
+package com.thevarungupta.spring_sample_app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping
+    public String index(){
+        return "Hello from spring boot app";
+    }
+}
---
+++ src/main/java/com/rndexercise/spring/sampe/app/SpringSampeAppApplication.java
@@ -0,0 +1,13 @@
+package com.rndexercise.spring.sampe.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSampeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSampeAppApplication.class, args);
+	}
+
+}
